eaafbaa7ff1700605eef3862fa64768426daeeb5
Ying-Xian Goh <146209106+yingxian-g@users.noreply.github.com>
2024-11-04 16:10:49 -0500

    Add files via upload

---
+++ micro_g2/code/spades.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Modified by Ying-Xian and Saehah for single-end SPAdes assembly
+# 10-20-24
+
+#SBATCH -J yx_spades_single_end # job name
+#SBATCH --account=introtogds
+#SBATCH --partition=normal_q
+#SBATCH --time=2-00:00:00 # time: days-hours:minutes:seconds
+#SBATCH --mem=128G # based on memory used when testing with interactive jobs
+#SBATCH --mail-user=yingxian@vt.edu #enter desired email address for updates
+#SBATCH --mail-type=BEGIN #include to get emailed when job begins
+#SBATCH --mail-type=END #include to get emailed when job ends
+#SBATCH --mail-type=FAIL #include to get emailed if job fails
+
+# Define paths
+data_dir="/projects/intro2gds/I2GDS2024/micro_g2/results/fastp_out_yingxian/"
+output_dir="/projects/intro2gds/I2GDS2024/micro_g2/results/spades_out_yingxian/"
+spades_path="/projects/leaph/.pyenv/versions/mambaforge/bin/spades.py"  # Updated path to spades.py
+
+# Create output directories if they don't exist
+mkdir -p $output_dir/contigs
+mkdir -p $output_dir/scaffolds
+
+# Change to the data directory
+cd $data_dir
+
+# Run SPAdes for each SE file
+for f in *_QC.fastq.gz
+do
+    # Check if _contigs.fasta already exists
+    if [ -f ${f%_QC.fastq.gz}/contigs.fasta ]; then
+        echo "_contigs.fasta exists for ${f}, skipping SPAdes..."
+    else
+        # Assemble using SPAdes with --isolate for single-end data
+        echo "Assembling ${f} with SPAdes"
+        python $spades_path --isolate -s $f -o ${f%_QC.fastq.gz} -t 3 -m 60
+
+        # Check the log file for issues
+        echo "Check the log file for ${f%_QC.fastq.gz} for any issues."
+    fi
+
+    # Move the final contigs and scaffolds to the output directory
+    if [ -f ${f%_QC.fastq.gz}/contigs.fasta ]; then
+        mv ${f%_QC.fastq.gz}/contigs.fasta $output_dir/contigs/${f%_QC.fastq.gz}_contigs.fasta
+    else
+        echo "contigs.fasta not found for ${f}, skipping move step..."
+    fi
+
+    if [ -f ${f%_QC.fastq.gz}/scaffolds.fasta ]; then
+        mv ${f%_QC.fastq.gz}/scaffolds.fasta $output_dir/scaffolds/${f%_QC.fastq.gz}_scaffolds.fasta
+    else
+        echo "scaffolds.fasta not found for ${f}, skipping move step..."
+    fi
+
+done
+
